8aeb786fbd2bc6d192b7f6faa64a7fc8c3078397
Sulaiman Faqih <jhokamfaqih5@gmail.com>
2025-02-14 21:55:05 +0700

    initial commit 9

--- src/App.jsx
+++ src/App.jsx
@@ -5,92 +5,125 @@ import "./App.css";
 const posts = [
   {
     id: 1,
-    type: "video",
-    src: "./assets/asset_majalengka/VID-20250212-WA0003.mp4",
-    caption: "Video Depan Rumah",
+    type: "image",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0033.jpg",
+    caption: "Foto sisi halaman depan rumah.",
   },
   {
     id: 2,
-    type: "video",
-    src: "./assets/asset_majalengka/VID-20250213-WA0001.mp4",
-    caption: "Video Dalam Rumah",
+    type: "image",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0034.jpg",
+    caption: "Foto sisi halaman depan rumah.",
   },
   {
     id: 3,
-    type: "video",
-    src: "./assets/asset_majalengka/VID-20250212-WA0001.mp4",
-    caption: "Video Dari Sdn Sukamulya",
+    type: "image",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0039.jpg",
+    caption: "Foto sisi kanan rumah.",
   },
   {
     id: 4,
-    type: "video",
-    src: "./assets/asset_majalengka/VID-20250212-WA0002.mp4",
-    caption: "Video Depan Rumah",
+    type: "image",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0035.jpg",
+    caption: "Foto sisi kiri rumah.",
   },
   {
     id: 5,
-    type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0025.jpg",
-    caption: "Foto samping rumah.",
+    type: "video",
+    src: "src/assets/asset_majalengka/0214(1).mp4",
+    caption:
+      "Video record halaman depan rumah, yang mana berhadapan langsung dengan jalan dan SDN Sukamulya 1.",
   },
   {
     id: 6,
-    type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0036.jpg",
-    caption: "Foto samping rumah.",
+    type: "video",
+    src: "src/assets/asset_majalengka/0214(2).mp4",
+    caption:
+      "Video record halaman depan rumah sampai ke belakang rumah, dengan luas tanah 19,5 Bata.",
   },
   {
     id: 7,
-    type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0027.jpg",
-    caption: "foto.",
+    type: "video",
+    src: "src/assets/asset_majalengka/0214.mp4",
+    caption: "Video record dari SDN Sukamulya 1 menuju rumah.",
   },
   {
     id: 8,
-    type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0038.jpg",
-    caption: "foto.",
+    type: "video",
+    src: "src/assets/asset_majalengka/0214(3).mp4",
+    caption: "Video record kondisi dalam rumah.",
   },
   {
     id: 9,
     type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0032.jpg",
-    caption: "foto.",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0036.jpg",
+    caption: "Foto.",
   },
   {
     id: 10,
     type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0035.jpg",
-    caption: "foto.",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0037.jpg",
+    caption: "Foto.",
   },
   {
     id: 11,
     type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0036.jpg",
-    caption: "foto.",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0031.jpg",
+    caption: "Foto.",
   },
   {
     id: 12,
     type: "image",
-    src: "./assets/asset_majalengka/IMG-20250212-WA0040.jpg",
-    caption: "foto.",
+    src: "src/assets/asset_majalengka/IMG-20250212-WA0038.jpg",
+    caption: "Foto.",
   },
 ];
 
 const App = () => {
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const handlePostClick = (post) => {
-    setSelectedPost(post);
-  };
-
-  const handleCloseModal = () => {
-    setSelectedPost(null);
-  };
+  const handlePostClick = (post) => setSelectedPost(post);
+  const handleCloseModal = () => setSelectedPost(null);
 
   return (
     <div className="app">
       <h1 className="title">Dijual Rumah</h1>
+
+      <div className="header">
+        <ul>
+          <li>
+            <span>Luas Tanah:</span> 19,5 Bata.
+          </li>
+          <li>
+            <span>Sertifikat Hak Milik (SHM):</span> a.n SAMSURI.
+          </li>
+          <li>
+            <span>Lokasi:</span> Desa Sukamulya, Kertajati, Majalengka, Jawa
+            Barat.
+          </li>
+          <li>
+            <span>Google Maps:</span>
+            <a
+              href="https://maps.app.goo.gl/yw4TY8gqqbbXa6ig9"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              https://maps.app.goo.gl
+            </a>
+          </li>
+          <li>
+            <span>Jarak ke Bandara Internasional:</span> 12 menit.
+          </li>
+          <li>
+            <span>Jarak ke Tol Cipali:</span> 15 menit.
+          </li>
+          <li>
+            <span>Hubungi:</span> 0812-1929-9086 (SAMSURI)
+          </li>
+        </ul>
+      </div>
+
       <div className="post-grid">
         {posts.map((post) => (
           <PostCard
@@ -116,7 +149,7 @@ const App = () => {
             )}
             <p>{selectedPost.caption}</p>
             <button className="close-button" onClick={handleCloseModal}>
-              Close
+              X
             </button>
           </div>
         </div>
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -5,13 +5,16 @@ const PostCard = ({ type, src, caption, onClick }) => {
   return (
     <div className="post-card" onClick={onClick}>
       {type === "image" ? (
-        <img src={src} alt={caption} className="post-image" />
+        <div className="post-card-overlay">
+          <img src={src} alt={caption} className="post-image" />
+          <div className="overlay-text">Lihat</div>
+        </div>
       ) : (
         <div className="video-wrapper">
           <video className="post-video" muted>
             <source src={src} type="video/mp4" />
           </video>
-          <div className="play-icon">▶</div>
+          <div className="play-icon">Tonton</div>
         </div>
       )}
     </div>
